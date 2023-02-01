9d1ca50ef57beaa536307358ed2a0c4436731422
Casey Becking <me@caseybecking.com>
2023-02-01 07:39:44 -0800

    updating group file to match the prefered output

--- group.py
+++ group.py
@@ -1,37 +1,65 @@
-import json
+import csv
+from requests import HTTPError as errors
 from admin_auth import _admin_auth
 from helper import get_google_config_dict
 
-def get_groups():
-        config_details = get_google_config_dict()
-        customer_id = config_details['customer_id'] 
-        service = _admin_auth()
-        data = []
-        response_group = service.groups().list(customer=customer_id).execute()
-        
-        for group in response_group['groups']:           
-            group_key = group['id']
-            group_email = group['email']
-            group_name = group['name']
-            group_description = group['description']
-            item = {}
-            item[group_email] = [{"group_details":{"group_email": group_email, "group_name": group_name, "group_description": group_description}}]
-            group_members = service.members().list(groupKey=group_key).execute()
-            _members = []
-            if 'members' in group_members:
-                for members in group_members['members']:
-                    member_email = str(members['email']) if 'email' in members else "No Email"
-                    member_status = str(members['status']) if 'status' in members else "No Status"
-                    member_role = str(members['role']) if 'role' in members else "No Role"
-                    _members.append({"member_email":member_email,"member_status":member_status,"member_role":member_role})
-                
-                item[group_email].extend(_members)
-            else:
-                item[group_email] = []
-                
-            data.append(item)
-        jsonDatas=json.dumps(data)
-        return(jsonDatas)
 
-if __name__ == '__main__':
-    print(get_groups())
+directory_service = _admin_auth()
+config_details = get_google_config_dict()
+customer_id = config_details['customer_id']
+all_groups = []
+all_members = []
+page_token = None
+new_page_token = None
+params = {'customer': customer_id}
+data_list = []
+
+while True:
+  try:
+    if page_token:
+      params['pageToken'] = page_token
+    current_page = directory_service.groups().list(**params).execute()
+
+    all_groups.extend(current_page['groups'])
+    page_token = current_page.get('nextPageToken')
+    if not page_token:
+      break
+  except errors.HttpError as error:
+    print('An error occurred: %s' % error)
+    break
+
+for group in all_groups:
+  group_key = group['email']
+  new_params = {'groupKey': '%s' % group_key, 'maxResults': '500'}
+
+  if group['directMembersCount'] == '0':
+    continue
+  else:
+    while True:
+      try:
+        if new_page_token:
+          new_params['pageToken'] = new_page_token
+        new_current_page = directory_service.members().list(**new_params).execute()
+        all_members.extend(new_current_page['members'])
+        new_page_token = new_current_page.get('nextPageToken')
+
+        if not new_page_token:
+          break
+      except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        break
+
+  
+  for member in all_members:
+    if 'email' in member:
+      row = group['email'],group['name'],group['description'],member['email'],member['role']
+      data_list.extend([row])
+    else:
+      row = group['email'],group['name'],group['description'],'NO EMAIL',member['role']
+      data_list.extend([row])
+  del all_members[:]
+
+with open("Output.csv", "w", newline="") as Output_File:
+  writer = csv.writer(Output_File,quotechar='|', quoting=csv.QUOTE_MINIMAL)
+  writer.writerow(['Group Email','Group Name','Group Description','Member Email','Member Role'])
+  writer.writerows(data_list)
